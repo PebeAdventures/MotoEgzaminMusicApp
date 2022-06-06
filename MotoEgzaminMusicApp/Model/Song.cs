c4d9485f013a182cdf6084074276f2a858f2423e
Piotr Bujak <piotr.bujak@motorolasolutions.com>
2022-06-06 12:58:55 +0200

    Add project files.

---
+++ MotoEgzaminMusicApp/Model/Song.cs
@@ -0,0 +1,20 @@
+﻿namespace MotoEgzaminMusicApp
+{
+
+
+    public class Song
+    {
+        private string Title { get; set; }
+        private string Author { get; set; }
+        private string AlbumName { get; set; }
+        private double Length { get; set; }
+
+        public Song(string title, string author, string albumName, double length)
+        {
+            Title = title;
+            Author = author;
+            AlbumName = albumName;
+            Length = length;
+        }
+    }
+}
---
+++ MotoEgzaminMusicApp/View/SongDisplay.cs
@@ -0,0 +1,6 @@
+﻿namespace MotoEgzaminMusicApp
+{
+    public class SongDisplay
+    {
+    }
+}
